bb33cf769196993ec4734095eec791245e60f49e
PokeCardShop <theodesiles@gmail.com>
2025-06-24 00:30:39 +0400

    Delete components/ui directory

--- components/ui/button.js
+++
@@ -1,7 +0,0 @@
-export function Button({ children, ...props }) {
-  return (
-    <button className="px-4 py-2 rounded bg-yellow-400 text-white hover:bg-yellow-500" {...props}>
-      {children}
-    </button>
-  );
-}
