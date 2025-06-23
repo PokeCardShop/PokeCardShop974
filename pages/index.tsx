0c346e85b70129201400eceb576cf2824d63121b
PokeCardShop <theodesiles@gmail.com>
2025-06-24 01:06:42 +0400

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-yellow-100">
-      <h1 className="text-4xl font-bold text-center">Bienvenue sur PokeCardShop 🌴⚡</h1>
-    </main>
-  );
-}
